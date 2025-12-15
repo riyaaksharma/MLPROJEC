81e9dee97bc744bad2892614eb5a45f3c0b389f4
Riya <sharmariya50052@gmail.com>
2025-12-15 12:39:33 +0530

    Data transformation is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 #whnever we are performng data indegstion component there should be some inputs(where to save train data,test data etc) that probably required by the data indestion component so for those inputs we need to create another class :
 @dataclass
@@ -58,6 +61,9 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
