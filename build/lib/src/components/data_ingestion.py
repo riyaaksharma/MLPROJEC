80e81a9b5dc81c742e5a0b6454add61a18b535d0
Riya <sharmariya50052@gmail.com>
2025-12-16 23:58:09 +0530

    The training part is done

--- build/lib/src/components/data_ingestion.py
+++ build/lib/src/components/data_ingestion.py
@@ -6,7 +6,16 @@ sys.path.append(str(Path(__file__).parent.parent))
 from exception import CustomException
 from logger import logging
 import pandas as pd
-from src.components import data_in
+from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer 
+
+
+
+
 
 
 from sklearn.model_selection import train_test_split
@@ -58,4 +67,10 @@ class DataIndegstion:
 
 if __name__ == "__main__":
     obj = DataIndegstion()
-    obj.initiatie_data_ingestion()
+    train_data,test_data=obj.initiatie_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
