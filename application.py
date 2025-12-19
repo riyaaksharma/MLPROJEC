e079601b6d45a71228a5cc8f25cc530befb1f91f
Riya <sharmariya50052@gmail.com>
2025-12-19 19:45:50 +0530

    Re-initialized application.py

--- application.py
+++ application.py
@@ -1,40 +1,37 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
-application=Flask(__name__)
-app=application
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-# Route for homepage
+# IMPORTANT: Flask object MUST be named `application`
+application = Flask(__name__)
 
-@app.route('/')
+# Home page
+@application.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+# Prediction route
+@application.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('home.html')
     else:
-        data=CustomData(
+        data = CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('race_ethnicity'),
             lunch=request.form.get('lunch'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
-
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('reading_score')),
             writing_score=float(request.form.get('writing_score'))
         )
 
-        pred_df=data.get_data_as_dataframe()
+        pred_df = data.get_data_as_dataframe()
         print(pred_df)
 
-        predict_pipeline=PredictPipeline()
+        predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
 
+        return render_template('home.html', results=results[0])
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
